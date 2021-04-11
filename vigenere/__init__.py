7b170a07ffb0818faad01c1839942756aaf46454
Ayush Gupta <darthvader01502@gmail.com>
2021-04-11 13:17:03 +0530

    Version 1.0.2

--- vigenere/__init__.py
+++ vigenere/__init__.py
@@ -7,4 +7,4 @@ with open("vigenere\info.txt", "r") as _info:
 __all__ = ["decrypt", "encrypt", "randint", "random_key", "valid_key"]
 __author__ = "Ayush Gupta; https://github.com/GuptaAyush19/"
 __license__ = "MIT License"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
